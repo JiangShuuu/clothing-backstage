efda64dd8fd9d8c2b71ce7e410c9214c719a4973
JiangShuuu <globelex65@gmail.com>
2022-07-26 22:29:02 +0800

    upd form

---
+++ layouts/forLogin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginLayout'
+}
+</script>
+
+<style scoped>
+.main {
+  padding: 10px;
+}
+</style>
